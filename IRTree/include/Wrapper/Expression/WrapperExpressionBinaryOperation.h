2f2de035ccd7a6ba05885aca2d67dd5ea75a657b
Zinenko Dmytro <carakanz@gmail.com>
2019-12-23 13:22:08 +0300

    Wrapper fix

--- IRTree/include/Wrapper/Expression/WrapperExpressionBinaryOperation.h
+++ IRTree/include/Wrapper/Expression/WrapperExpressionBinaryOperation.h
@@ -15,11 +15,60 @@ namespace IRTree {
                 : left_(std::move(left)), right_(std::move(right)) {
         }
 
-        std::unique_ptr<IExpressionNode> to_expression() override { assert(false); }
+        std::unique_ptr<IExpressionNode> to_expression() override {
+            return std::make_unique<ExpressionBinaryOperationNode>(operation_type,
+                                                                   left_->to_expression(),
+                                                                   right_->to_expression());
+        }
 
-        std::unique_ptr<IStatementNode> to_statement() override { assert(false); }
+        std::unique_ptr<IStatementNode> to_statement() override {
+            return std::make_unique<StatementSeqNode>(left_->to_statement(),
+                                                      right_->to_statement());
+        }
 
-        std::unique_ptr<IStatementNode> to_conditional() override { assert(false); }
+        std::unique_ptr<IStatementNode> to_conditional(std::unique_ptr<LabelNode>& true_label,
+                                                       std::unique_ptr<LabelNode>& false_label) override {
+            switch (operation_type) {
+                case BinaryOperationType::EQUIVALENT:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::EQUIVALENT,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(true_label),
+                                                                std::move(false_label));
+                case BinaryOperationType::NOT_EQUIVALENT:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::NOT_EQUIVALENT,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(true_label),
+                                                                std::move(false_label));
+                case BinaryOperationType::LESS:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::LESS,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(true_label),
+                                                                std::move(false_label));
+                case BinaryOperationType::LESS_OR_EQUIVALENT:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::LESS_OR_EQUIVALENT,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(true_label),
+                                                                std::move(false_label));
+                case BinaryOperationType::MORE:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::LESS_OR_EQUIVALENT,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(false_label),
+                                                                std::move(true_label));
+                case BinaryOperationType::MORE_OR_EQUIVALENT:
+                    return std::make_unique<StatementCJumpNode>(CJumpCondition::LESS,
+                                                                left_->to_expression(),
+                                                                right_->to_expression(),
+                                                                std::move(false_label),
+                                                                std::move(true_label));
+                default:
+                    assert(false);
+            }
+        }
 
     private:
         std::unique_ptr<IWrapper> left_;
--- IRTree/include/Wrapper/Wrapper.h
+++ IRTree/include/Wrapper/Wrapper.h
@@ -8,21 +8,33 @@
 #include "cassert"
 
 namespace IRTree {
+
     template<class NodeType>
     class Wrapper : public IWrapper {
     public:
         explicit Wrapper(std::unique_ptr<NodeType> node) : node_(std::move(node)) {
         }
 
+//        template<typename T = std::enable_if_t<std::is_base_of_v<IExpressionNode,NodeType>>>
+//        inline std::unique_ptr<IExpressionNode> to_expression_check() {
+//            return std::move(node_);
+//        }
+        template<typename T>
+        std::unique_ptr<IExpressionNode> to_expression_check();
+
+        
+
         std::unique_ptr<IExpressionNode> to_expression() override {
-            assert(false);
+            //return to_expression_check<int>();
+            return std::move(dynamic_cast<>);
         }
 
         std::unique_ptr<IStatementNode> to_statement() override {
-            assert(false);
+            assert();
         }
 
-        std::unique_ptr<IStatementNode> to_conditional() override {
+        std::unique_ptr<IStatementNode> to_conditional(std::unique_ptr<LabelNode> & /*true_label*/,
+                                                       std::unique_ptr<LabelNode> & /*false_label*/) override {
             assert(false);
         }
 
